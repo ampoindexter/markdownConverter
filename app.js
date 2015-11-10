193c2d596de867bd5d81af4217130185e47a59fd
Alicia Poindexter <alcampbel@gmail.com>
2015-11-10 00:10:50 -0800

    updated port

--- app.js
+++ app.js
@@ -25,5 +25,5 @@ app.post('/convert', function(req, res) {
   res.send(outputText);
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.listen(PORT);
+// console.log('Listening on port 3000');
